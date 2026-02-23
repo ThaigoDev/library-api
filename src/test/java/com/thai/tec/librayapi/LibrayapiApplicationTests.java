cf88dc6926f31886ed4fdfe2ccb61af23673e4d3
ThaigoDev <thaigodeveloper@gmail.com>
2026-02-23 09:51:35 -0300

    chore: configs

---
+++ src/test/java/com/thai/tec/librayapi/LibrayapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thai.tec.librayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
