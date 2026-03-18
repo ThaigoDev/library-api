4b0c85aeb33d4bad83139d2392bbc791625f0c3d
ThaigoDev <thaigodeveloper@gmail.com>
2026-03-18 15:06:30 -0300

    token config

--- src/main/java/com/thai/tec/librayapi/config/AuthorizationServerConfiguration.java
+++ src/main/java/com/thai/tec/librayapi/config/AuthorizationServerConfiguration.java
@@ -5,22 +5,30 @@ import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.thai.tec.librayapi.security.CustomAuthentication;
+import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Duration;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Configuration
@@ -94,5 +102,32 @@ public class AuthorizationServerConfiguration {
                 .jwkSetEndpoint("/oauth2/jwks")
                 .oidcLogoutEndpoint("/oauth2/logout")
                 .build();
+    }
+
+    @Bean
+     public OAuth2TokenCustomizer<JwtEncodingContext> tokenCustomizer() {
+        return context -> {
+            //aqui pegamos o principal do contexto da autenticação
+            var principal = context.getPrincipal();
+            //verificamos se a autenticação é uma instancia de CustomAuthentication
+            if(principal instanceof CustomAuthentication authentication) {
+            //aqui pegamos o tipo do token
+                OAuth2TokenType tipoToken = context.getTokenType();
+                // verificamos se o tipo do token é de ACCESS TOKEN
+                //ACCESS TOKEN : token que é enviado na requisição
+                //REFRESH TOKEN : é um grant type, o refresh token emite um novo access token sem perder
+                // a seção atual.
+                if(OAuth2TokenType.ACCESS_TOKEN.equals(tipoToken)) {
+                    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+                    List<String> list = authorities.stream().map(GrantedAuthority::getAuthority).toList();
+                   //Claims são as propriedades do token, aqui passamos as claims que queremos.
+                    context
+                            .getClaims()
+                            .claim("authorities",list );
+                }
+
+            }
+        };
     }
 }
